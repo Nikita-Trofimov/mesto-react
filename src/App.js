aaf28356d4264cff55ba18167695fe6b1c48dfa4
Nikita Trofimov <nikita.igorevi4@yandex.ru>
2021-04-15 19:06:00 +0300

    создан компонент для попапа с формой

--- src/App.js
+++ src/App.js
@@ -11,9 +11,25 @@ function App() {
         <Header />
         <Main />
         <Footer />
-        <PopupWithFrom title="Редактировать профиль" name="edit-profile"/>
-        <PopupWithFrom title="Новое место" name="add-card"/>
-        <PopupWithFrom title="Обновить аватар" name="edit-avatar"/>
+        <PopupWithFrom title="Редактировать профиль" name="edit-profile">
+          <input name="name" type="text" placeholder="Имя" defaultValue="" className="popup__input popup__input_type_name" required minLength="2" maxLength="40" />
+          <span className="name-input-error popup__error-message"></span>
+          <input name="proffesion" type="text" placeholder="Занятие" defaultValue="" className="popup__input popup__input_type_profession" required minLength="2" maxLength="200" />
+          <span className="proffesion-input-error popup__error-message"></span>
+          <button type="submit" className="popup__submit-button">Сохранить</button>
+        </PopupWithFrom>
+        <PopupWithFrom title="Новое место" name="add-card">
+          <input name="name" type="text" placeholder="Название" className="popup__input popup__input_type_name" required minLength="2" maxLength="30" />
+          <span className="name-input-error popup__error-message"></span>
+          <input name="image" type="url" placeholder="Ссылка на картинку" className="popup__input popup__input_type_card-link" required />
+          <span className="image-input-error popup__error-message"></span>
+          <button type="submit" className="popup__submit-button">Создать</button>
+        </PopupWithFrom>
+        <PopupWithFrom title="Обновить аватар" name="edit-avatar">
+          <input name="image" type="url" placeholder="Ссылка на картинку" className="popup__input popup__input_type_avtar-link" required />
+          <span className="image-input-error popup__error-message"></span>
+          <button type="submit" className="popup__submit-button">Сохранить</button>
+        </PopupWithFrom>
           <section className="popup popup-illustration-container">
             <figure className="popup-illustration">
                 <img className="popup-illustration__img" alt="фото иллюстрации" src="images/karachaevsk.jpg"/>
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -5,51 +5,11 @@ function PopupWithFrom(props) {
         <div className="popup__container">
           <h2 className="popup__title">{props.title}</h2>
           <form name={props.name} className="popup__form" noValidate>
-            <input name="name" type="text" placeholder="Имя" defaultValue="" className="popup__input popup__input_type_name" required minLength="2" maxLength="40" />
-            <span className="name-input-error popup__error-message"></span>
-            <input name="proffesion" type="text" placeholder="Занятие" defaultValue="" className="popup__input popup__input_type_profession" required minLength="2" maxLength="200" />
-            <span className="proffesion-input-error popup__error-message"></span>
-            <button type="submit" className="popup__submit-button">Сохранить</button>
+            {props.children}
           </form>
           <button type="button" className="popup__close-button"></button>
         </div>
       </section>
-{/* 
-      <section className="popup popup-add-card">
-        <div className="popup__container">
-          <h2 className="popup__title">Новое место</h2>
-          <form name="add-card" className="popup__form" noValidate>
-            <input name="name" type="text" placeholder="Название" className="popup__input popup__input_type_name" required minLength="2" maxLength="30" />
-            <span className="name-input-error popup__error-message"></span>
-            <input name="image" type="url" placeholder="Ссылка на картинку" className="popup__input popup__input_type_card-link" required />
-            <span className="image-input-error popup__error-message"></span>
-            <button type="submit" className="popup__submit-button">Создать</button>
-          </form>
-          <button type="button" className="popup__close-button"></button>
-        </div>
-      </section>
-
-      <section className="popup popup-edit-avatar">
-        <div className="popup__container">
-          <h2 className="popup__title">Обновить аватар</h2>
-          <form name="add-card" className="popup__form" noValidate>
-            <input name="image" type="url" placeholder="Ссылка на картинку" className="popup__input popup__input_type_avtar-link" required />
-            <span className="image-input-error popup__error-message"></span>
-            <button type="submit" className="popup__submit-button">Сохранить</button>
-          </form>
-          <button type="button" className="popup__close-button"></button>
-        </div>
-      </section>
-
-      <section className="popup popup-remove-confirm">
-        <div className="popup__container">
-          <h2 className="popup__title popup__title-confirm">Вы уверены?</h2>
-          <form name="confirm" className="popup__form">
-            <button type="submit" className="popup__submit-button popup__submit-button-confirm">Да</button>
-          </form>
-          <button type="button" className="popup__close-button"></button>
-        </div>
-      </section> */}
     </>
   )
 }
