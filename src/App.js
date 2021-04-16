2b0856bbfdaa3e5e9c770b0a93f8d09f269ab99a
Nikita Trofimov <nikita.igorevi4@yandex.ru>
2021-04-16 23:49:08 +0300

    исправлена опечатка в компоненте PopupWithForm

--- src/App.js
+++ src/App.js
@@ -41,25 +41,25 @@ function App() {
               onEditAvatar={handleEditAvatarClick}
               onCardClick={handleCardClick}/>
         <Footer />
-        <PopupWithFrom title="Редактировать профиль" name="edit-profile" isOpen={isEditProfilePopupOpen} onClose={closeAllPopups}>
+        <PopupWithForm title="Редактировать профиль" name="edit-profile" isOpen={isEditProfilePopupOpen} onClose={closeAllPopups}>
           <input name="name" type="text" placeholder="Имя" defaultValue="" className="popup__input popup__input_type_name" required minLength="2" maxLength="40" />
           <span className="name-input-error popup__error-message"></span>
           <input name="proffesion" type="text" placeholder="Занятие" defaultValue="" className="popup__input popup__input_type_profession" required minLength="2" maxLength="200" />
           <span className="proffesion-input-error popup__error-message"></span>
           <button type="submit" className="popup__submit-button">Сохранить</button>
-        </PopupWithFrom>
-        <PopupWithFrom title="Новое место" name="add-card" isOpen={isAddPlacePopupOpen} onClose={closeAllPopups}>
+        </PopupWithForm>
+        <PopupWithForm title="Новое место" name="add-card" isOpen={isAddPlacePopupOpen} onClose={closeAllPopups}>
           <input name="name" type="text" placeholder="Название" className="popup__input popup__input_type_name" required minLength="2" maxLength="30" />
           <span className="name-input-error popup__error-message"></span>
           <input name="image" type="url" placeholder="Ссылка на картинку" className="popup__input popup__input_type_card-link" required />
           <span className="image-input-error popup__error-message"></span>
           <button type="submit" className="popup__submit-button">Создать</button>
-        </PopupWithFrom>
-        <PopupWithFrom title="Обновить аватар" name="edit-avatar" isOpen={isEditAvatarPopupOpen} onClose={closeAllPopups}>
+        </PopupWithForm>
+        <PopupWithForm title="Обновить аватар" name="edit-avatar" isOpen={isEditAvatarPopupOpen} onClose={closeAllPopups}>
           <input name="image" type="url" placeholder="Ссылка на картинку" className="popup__input popup__input_type_avtar-link" required />
           <span className="image-input-error popup__error-message"></span>
           <button type="submit" className="popup__submit-button">Сохранить</button>
-        </PopupWithFrom>
+        </PopupWithForm>
         <ImagePopup card={selectedCard} onClose={closeAllPopups}/>
       </div>
     </div>
