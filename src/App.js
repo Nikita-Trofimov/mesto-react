d4e7ceb6bd783a5ea59c6434a7b504d9278fdf74
Nikita Trofimov <nikita.igorevi4@yandex.ru>
2021-04-16 23:51:59 +0300

    исправлена опечатка в компоненте PopupWithForm | исправлен export/import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Main from "./components/Main";
-import PopupWithFrom from "./components/PopupWithForm";
+import PopupWithForm from "./components/PopupWithForm";
 import ImagePopup from "./components/ImagePopup";
 import React from 'react';
 
